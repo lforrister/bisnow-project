5bcba23eebfc20a818ec8f8910d3991ba645f556
Lindsay Forrister <lindsayforrister@gmail.com>
2021-10-04 10:24:26 -0500

    More setup stuff

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EmailDirectory />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmailDirectory from './components/EmailDirectory.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmailDirectory,
   }
 }
 </script>
---
+++ src/components/EmailDirectory.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1> TESTING SETUP </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Email Directory',
+}
+</script>
+
+<style lang="scss">
+</style>
