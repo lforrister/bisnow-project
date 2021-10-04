7aabe5ce2b28f116c5582ce9d12e814fb98d2515
Lindsay Forrister <lindsayforrister@gmail.com>
2021-10-04 10:32:20 -0500

    Getting some basics for the email input stuff in there

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="min-h-screen bg-gradient-to-br from-blue-200 flex justify-center flex-col p-5">
     <EmailDirectory />
   </div>
 </template>
@@ -20,8 +20,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/EmailDirectory.vue
+++ src/components/EmailDirectory.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    <h1> TESTING SETUP </h1>
-  </div>
+    <div class="bg-white max-w-lg mx-auto p-8 bg-white md:shadow-md rounded-md">
+        <h1> Email Directory </h1>
+        <p>
+            Please enter an email in the field below to retrieve the associated information.
+        </p>
+
+        <form>
+            <div>
+                <label>
+                    Email Address
+                </label>
+                <input
+                v-model="emailInput"
+                id="email"
+                type="email"
+                placeholder="Email"
+                required
+                />
+            </div>
+
+            <button
+                type="submit"
+                @click.prevent="emailSearch()"
+            >
+                Submit
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'Email Directory',
+  methods: {
+    emailSearch() {
+      //Here is where we'll search through the directory
+      console.log('searching!')
+    }
+  }
 }
 </script>
 
